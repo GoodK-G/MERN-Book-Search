eb344dc30e1b796377fb24b402a97c1b608ecca1
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2024-02-14 11:07:56 -0600

    Changes to the server.js file, addded middleware for the GraphQL apollo server, and schemas folder structure created.

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,14 +1,20 @@
 const jwt = require('jsonwebtoken');
+const {GraphQLError} = require('qraphql');
 
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  AuthenticationError: new GraphQLError('could not authenticate user.',{
+    extensions:{
+      code: 'UNAUTHENTICATED',
+    },
+  }),
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
